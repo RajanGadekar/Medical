fcb15a4d30ad5276d0ad70bc5736ef4503709b04
RajanGadekar <rajan.gadekar2015@gmail.com>
2024-01-29 23:03:24 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Route path="contact" element={<Contact />} />
           <Route path="blogs" element={<Blogs />} />
       </Routes>
+      {/* <Footer/> */}
     </BrowserRouter>
   );
 }
